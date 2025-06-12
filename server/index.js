1979f0b3bcbdee17ef820d7882b4e57bd1593dbc
Aryan Manav <92841408+AryanManav@users.noreply.github.com>
2025-06-12 15:36:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -81,4 +81,4 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => console.log('Server running on http://localhost:${PORT}'));
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
